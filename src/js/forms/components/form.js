efb6dd85ae63c631cdd57d3c92cc6cb08be77251
Eric Valadas <eric@playgroundinc.com>
2016-08-22 10:02:59 -0400

    Keep track of number of invalid fields in the form

--- src/js/forms/components/form.js
+++ src/js/forms/components/form.js
@@ -5,6 +5,12 @@ export default React.createClass({
   children: [],
   fields: [],
 
+  getInitialState() {
+    return {
+      numberInvalid: 0
+    };
+  },
+
   componentWillMount() {
     this.children = React.Children.map(this.props.children, (child) => {
       if (child.type.displayName === "field") {
@@ -17,13 +23,15 @@ export default React.createClass({
   },
 
   validate() {
-    let valid = true;
+    let numberInvalid = 0;
+    this.setState({numberInvalid: 0});
     for (let field of this.fields) {
       if (!field.validate()) {
-        valid = false;
+        numberInvalid += 1;
       }
     }
-    return valid;
+    this.setState({numberInvalid: numberInvalid});
+    return numberInvalid === 0;
   },
 
   handleSubmit(e) {
@@ -36,6 +44,7 @@ export default React.createClass({
   render() {
     return (
       <form {...this.props} onSubmit={this.handleSubmit}>
+        {this.state.numberInvalid} Invalid
         {this.children}
       </form>
     );
