4ca3777d2f84f60158f34159f0f763a8c90f7530
Eric Valadas <ericvaladas@gmail.com>
2016-08-31 21:59:09 -0400

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 const gulp = require('gulp');
+const path = require('path');
 const webpackStream = require('webpack-stream');
 const mocha = require('gulp-mocha');
+const manifest = require('./package.json');
+
+const mainFile = manifest.main;
+const packageName = manifest.name;
+const destinationDirectory = path.dirname(mainFile);
+const exportFileName = path.basename(mainFile, path.extname(mainFile));
 
 
-
-function packLibrary(filename) {
-  return webpackStream({
-    externals: {
-      "react": "react"
-    },
-    output: {
-      filename: filename,
-      library: 'Formwood',
-      libraryTarget: 'umd'
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }]
-    }
-  });
-}
-
-function buildJavaScript(watch) {
+function build() {
   return gulp.src('./src/js/index.js')
-    .pipe(packLibrary('index.js'))
-    .pipe(gulp.dest('./lib'));
-}
-
+    .pipe(webpackStream({
+      externals: {
+        "react": "react"
+      },
+      output: {
+        filename: `${exportFileName}.js`,
+        library: packageName,
+        libraryTarget: 'umd'
+      },
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }]
+      }
+    }))
+    .pipe(gulp.dest(destinationDirectory));
 }
 
 function runMochaTests() {
@@ -41,7 +41,6 @@ function runMochaTests() {
   }));
 }
 
-
-gulp.task('build', buildJavaScript);
-gulp.task('default', ['build']);
+gulp.task('build', build);
 gulp.task('test', runMochaTests);
+gulp.task('default', ['build']);
