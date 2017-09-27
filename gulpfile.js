bea9744852ab9193a6a43691839b22949988a097
Eric Valadas <ericvaladas@gmail.com>
2017-09-27 13:02:32 -0400

    Use nyc for testing

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const webpackStream = require('webpack-stream');
 const babel = require('gulp-babel');
-const mocha = require('gulp-mocha');
-const istanbul = require('gulp-babel-istanbul');
 const coveralls = require('gulp-coveralls');
 const eslint = require('gulp-eslint');
 const manifest = require('./package.json');
@@ -24,25 +22,6 @@ function sendToCoveralls() {
     .pipe(coveralls());
 }
 
-function runMochaTests() {
-  return gulp.src('test/unit/**/*.js', {read: false})
-    .pipe(mocha({
-      reporter: 'dot',
-      ignoreLeaks: false
-    }));
-}
-
-function test() {
-  require('babel-core/register');
-  return gulp.src('src/**/*.js')
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', function() {
-      runMochaTests()
-        .pipe(istanbul.writeReports())
-    });
-}
-
 function lint() {
   return gulp.src('src/**/*')
     .pipe(eslint())
@@ -51,7 +30,6 @@ function lint() {
 }
 
 gulp.task('build', build);
-gulp.task('test', test);
 gulp.task('coveralls', sendToCoveralls);
 gulp.task('lint', lint);
 gulp.task('default', ['build']);
